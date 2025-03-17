8f8fc6b96ec10c37e0dabe18f4657ffcbc954657
Ji Liu <liuji1031@gmail.com>
2025-03-17 15:02:48 -0400

    feat: change input argument

--- ontology_to_schema/cli.py
+++ ontology_to_schema/cli.py
@@ -21,21 +21,28 @@ logger = logging.getLogger("ontology_to_schema")
 
 @app.command()
 def main(
-    inp_path: str = typer.Option(
+    inp_dir: str = typer.Option(
         ...,
-        "--inp_path",
+        "--inp_dir",
         "-i",
-        help="Path to the ontology file",
-        dir_okay=False,
+        help="Directory containing the ontology file",
+        file_okay=False,
         exists=True,
         readable=True,
         resolve_path=True,
     ),
-    out_path: str = typer.Option(
+    file_name: str = typer.Option(
         ...,
-        "--out_path",
+        "--file_name",
+        "-f",
+        help="Name of the ontology file",
+    ),
+    out_dir: str = typer.Option(
+        ...,
+        "--out_dir",
         "-o",
-        help="Path to the output schema file",
+        help="Path to save the output schema file",
+        file_okay=False,
         writable=True,
         resolve_path=True,
     ),
@@ -55,20 +62,23 @@ def main(
 ):
     """Command line interface for the ontology_to_schema package."""
     logger.setLevel(log_level)
+    inp_path = pathlib.Path(inp_dir) / file_name
+    assert inp_path.exists(), f"File {inp_path} does not exist"
     logger.info(f"Ontology file: {inp_path}")
-    logger.info(f"Output schema file: {out_path}")
+    logger.info(f"Output schema file: {out_dir}")
     logger.info(f"Agent configuration file: {agent_cfg}")
     logger.info(f"Log level: {log_level}")
 
     # create output folder if it does not exist
-    out_dir = pathlib.Path(out_path).parent
-    out_dir.mkdir(parents=True, exist_ok=True)
+    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     converter = OwlImportEngine(agent_cfg=agent_cfg, log_level=log_level)
-    schema = converter.convert(inp_path)
-    logger.info(f"Writing schema to {out_path}...")
-    write_schema(schema, out_path)
-    fix_class_definition_sequence(out_path, overwrite=True)
+    schema = converter.convert(str(inp_path))
+    logger.info(f"Writing schema to {out_dir}...")
+    stem = file_name.split(".")[0]
+    out_path = pathlib.Path(out_dir) / f"{stem}.yaml"
+    write_schema(schema, str(out_path))
+    fix_class_definition_sequence(str(out_path), overwrite=True)
     logger.info("✅ All done!")
 
 
