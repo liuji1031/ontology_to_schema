dd969ed09fe24d06fe6dfdff81b64ccb4344b0f6
Ji Liu <liuji1031@gmail.com>
2025-03-27 13:25:34 -0400

    feat: add ttl to ofn conversion

--- ontology_to_schema/util.py
+++ ontology_to_schema/util.py
@@ -1,5 +1,7 @@
 """Utility functions for the ontology_to_schema module."""
+import logging
 import os
+import pathlib
 
 import instructor
 
@@ -28,3 +30,50 @@ def setup_client(provider, api_key_env_varname):
         raise ValueError(f"Unsupported provider: {provider}")
 
     return client
+
+
+def ttl_to_ofn(ttl: str | list[str]) -> None:
+    """Convert Turtle file(s) to an OFN file.
+
+    Args:
+        ttl (str | list[str]): The Turtle file or list of Turtle files to
+        convert.
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    if isinstance(ttl, str):
+        file_list = [ttl]
+    else:
+        file_list = ttl
+
+    out_dir = None
+    err_files = []
+
+    for file in file_list:
+        if out_dir is None:
+            out_dir = pathlib.Path(file).parent / ".ofn"
+
+        filepath = pathlib.Path(file)
+        ofn_filepath = out_dir / pathlib.Path(file).name.replace(
+            ".ttl", ".ofn"
+        )
+
+        if ofn_filepath.exists():
+            logger.info(f"{str(ofn_filepath)} already exists. Skipping...")
+            continue
+
+        logger.info(f"Converting {filepath} to .ofn using ROBOT")
+        if os.system(
+            f"robot convert -i {str(filepath)} "
+            + f"-output {str(ofn_filepath)}"
+        ):
+            logger.error(f"Error converting {str(filepath)} to .ofn")
+            err_files.append(filepath)
+
+    logger.info(f"Conversion complete. OFN files are in {str(out_dir)}")
+
+    if err_files:
+        logger.error("The following files failed to convert:")
+        for f in err_files:
+            logger.error(str(f))
